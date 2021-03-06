5902c18563ca9180de611c496f2e508f86ce2f35
karina.v.99933@gmail.com <karina.v.99933@gmail.com>
2021-03-06 23:23:12 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let strAdd = String(str);
+  let result = '';
+  if (options.hasOwnProperty("addition")) {
+    strAdd += String(options.addition);
+  }
+  if (options.hasOwnProperty("additionRepeatTimes")) {
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+      strAdd += (options.additionSeparator || "|") + options.addition;
+    }
+  }
+  if (options.hasOwnProperty("repeatTimes")) {
+    for (let i = 1; i < options.repeatTimes; i++) {
+      result += strAdd + (options.separator || "+");
+    }
+    result += strAdd;
+  } else {
+    result = strAdd;
+  }
+  return result;
 };
-  

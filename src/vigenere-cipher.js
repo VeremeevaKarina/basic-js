e02de9b9c9e065b3c6a614423cfd427e2306bb6d
karina.v.99933@gmail.com <karina.v.99933@gmail.com>
2021-03-06 23:16:59 +0300

    feat: implement class VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.isDirect = direction;
+  }
+  encrypt(message, key) {
+    if(arguments.length <= 1) {
+      throw new Error("Uncorrect input data!");
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let cipherArr = [];
+    let index = 0;
+
+    for(let i = 0; i < message.length; i++) 
+    {
+        let code = message[i].charCodeAt(0);
+        if(code >= 65 && code <= 90) 
+        {
+            let letter = String.fromCharCode((code + key.charCodeAt(index)) % 26 + "A".charCodeAt(0));
+            cipherArr.push(letter);
+            if(index != key.length - 1) index++;
+            else index = 0;
+        }
+        else {
+          cipherArr.push(message[i])
+        };         
+    }
+    if(this.isDirect == false) {
+      cipherArr.reverse()
+    };
+    return cipherArr.join("");       
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if(arguments.length <= 1) {
+      throw new Error("Uncorrect input data!");
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let arr = [];
+    let index = 0;
+    for(let i = 0; i < encryptedMessage.length; i++) {
+        let code = encryptedMessage[i].charCodeAt(0);
+        if(code >= 65 && code <= 90) 
+        {
+            let letter = String.fromCharCode((code + 26 - key.charCodeAt(index)) % 26 + "A".charCodeAt(0));
+            arr.push(letter);
+            if(index != key.length - 1) index++;
+            else index = 0;
+        }
+        else arr.push(encryptedMessage[i]);         
+    }
+    if(this.isDirect == false) {
+      arr.reverse()
+    };
+    return arr.join('');
   }
 }
 

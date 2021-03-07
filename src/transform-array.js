6987c90946a2fac672985ccbd54d8f6e890f5311
karina.v.99933@gmail.com <karina.v.99933@gmail.com>
2021-03-07 17:37:45 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let result = [];
+  const prevCommands = ['--discard-prev', '--double-prev'];
+  const nextCommands = ['--discard-next', '--double-next'];
+ 
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (prevCommands.includes(element) || nextCommands.includes(element)) {
+      continue;
+    }
+
+    let prevElement = arr[i - 1];
+    let nextElement = arr[i + 1];
+
+    if (nextCommands.includes(prevElement)) {
+      if (prevElement == "--discard-next") {
+        continue;
+      } else {
+        result.push(element);
+      }
+    }
+
+    result.push(element);
+
+    if (prevCommands.includes(nextElement)) {
+      if (nextElement == "--discard-prev") {
+        result.pop();
+      } else {
+        result.push(element);
+      }
+    }
+  }
+
+  return result;
 };
